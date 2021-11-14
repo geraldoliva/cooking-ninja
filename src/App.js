80e63c6ffcef220935c06a2bad71b98b1e282e38
Gerald Oliva <geraldsamoliva@gmail.com>
2021-11-14 17:17:39 -0500

    Add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// page components
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+
+// styles
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
