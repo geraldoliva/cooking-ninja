218d907b5886ae27c568314e07e4c84c93bb6828
Gerald Oliva <geraldsamoliva@gmail.com>
2021-11-14 22:38:53 -0500

    Add POST request functionality

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,17 +1,37 @@
-import { useState } from 'react'
+import { useState, useRef } from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 // styles
-import './Create.css'
+import "./Create.css";
 
 export default function Create() {
-  const [title, setTitle] = useState('')
-  const [method, setMethod] = useState('')
-  const [cookingTime, setCookingTime] = useState('')
-  
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  );
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime)
-  }
+    postData({title, ingredients, method, cookingTime: cookingTime + 'minutes'})
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing]);
+    }
+    setNewIngredient("");
+    ingredientInput.current.focus();
+  };
 
   return (
     <div className="create">
@@ -20,7 +40,7 @@ export default function Create() {
       <form onSubmit={handleSubmit}>
         <label>
           <span>Recipe title:</span>
-          <input 
+          <input
             type="text"
             onChange={(e) => setTitle(e.target.value)}
             value={title}
@@ -28,7 +48,26 @@ export default function Create() {
           />
         </label>
 
-      {/* ingredients go here */}
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{" "}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
 
         <label>
           <span>Recipe method:</span>
@@ -40,7 +79,7 @@ export default function Create() {
         </label>
 
         <label>
-          <span>Cooking time (minutess):</span>
+          <span>Cooking time (minutes):</span>
           <input
             type="number"
             onChange={(e) => setCookingTime(e.target.value)}
@@ -52,5 +91,5 @@ export default function Create() {
         <button className="btn">Submit</button>
       </form>
     </div>
-  )
+  );
 }
