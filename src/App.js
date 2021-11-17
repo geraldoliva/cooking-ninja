aa0ca6354954f2fdc77d6de48925d5f6eb7823e8
Gerald Oliva <geraldsamoliva@gmail.com>
2021-11-17 16:43:58 -0500

    Add color theme selector

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Search from "./pages/search/Search";
 import Create from "./pages/create/Create";
 import Recipe from "./pages/recipe/Recipe";
 import Navbar from "./components/Navbar";
+import ThemeSelector from "./components/ThemeSelector";
 
 // styles
 import "./App.css";
@@ -15,6 +16,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <ThemeSelector />
         <Switch>
           <Route exact path="/">
             <Home />
